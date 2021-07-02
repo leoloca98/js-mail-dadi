f4ed17faf73f69dcf6c8a330b3620368ecd4ba1a
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-02 13:27:48 +0200

    Add template

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
+
+*/
+
+// !MAIL
+/*
+1 - Creare un array di 10 mail (stile whitelist) che useremo per confrontare con quella inserita dell'utente
+2 - Chidiamo all'utente di inserire la sua mail
+3 - Verifichiamo se la mail inserita è presente nella whitelist
+4 - Se è presente diamo un OK alla validazione della mail, altrimenti richiediamo di inserire un'altra mail dando un errore.
+
+*/
