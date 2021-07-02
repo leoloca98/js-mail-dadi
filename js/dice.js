fa5e18ec28da04d938686197099840ef1a26627c
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-02 14:35:16 +0200

    Add Dice.js and wrote the steps

---
+++ js/dice.js
@@ -0,0 +1,23 @@
+/*
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
+
+*/
+
+// !DADI
+/*
+
+1 - Creare una variabile per il giocatore e un'altra per il computer
+2 - Generare un numero casuale tra 1 e 6 (compresi) e assegnarlo al giocatore
+3 - Stessa cosa ma assegnarla al computer
+4 - Creare una variabile che servirà per il controllo del risultato
+5 - Sottrarre il numero ottenuto dal giocatore con quello ottenuto dal computer
+6 - Assegnare il risultato alla variabile citata nel punto 4
+7 - Se il risultato è maggiore di 0 allora il vincitore è il giocatore
+8 - Se il risultato è uguale a 0 allora ci sarà un pareggio
+9 - Se il risultato è minore di 0 la vittoria andrà al computer
+10 - Stampare il risulato ottenuto e la conseguente scitta "You Win / You Lose / Draw"
+
+*/
--- js/script.js
+++ js/script.js
@@ -5,10 +5,6 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
-
 */
 
 // !MAIL
